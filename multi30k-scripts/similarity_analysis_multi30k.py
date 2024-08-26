825c1d5f6c4ce8fd4e422b07172feddfbc4e1e9f
CWDE615 <cwellis001@gmail.com>
2024-08-26 15:13:24 -0400

    data analysis script working. No sbatch script yet because it runs fast enough on its own.

--- multi30k-scripts/similarity_analysis_multi30k.py
+++ multi30k-scripts/similarity_analysis_multi30k.py
@@ -2,44 +2,51 @@
 # Read cosine similarity from file for a language pair. Similarities must be calculated previous to the analysis.
 import argparse
 # import matplotlib.pyplot as plt # TODO: Uncomment this and set up matplotlib to make tables and histograms automatically.
-import numpy
+import numpy as np
+
+def loadtxt(filename):
+	return np.loadtxt(filename, dtype = 'd', delimiter = "|", comments = None)
 
 def get_mean_cosine_similarity(src_lang, tgt_lang, datasets):
-	accumulator = []
+	sums = []
+	num = 0
 
 	for dataset in datasets:
-                IN_FILE = f"multi30k-dataset-{src_lang}-{tgt_lang}/similarity_{dataset}_{src_lang}_{tgt_lang}.txt"
-		sims = np.loadtxt(IN_FILE, dtype = 'd')
-		accumulator.append(sims)
+		IN_FILE = f"multi30k-dataset-{src_lang}-{tgt_lang}/similarity_{dataset}_{src_lang}_{tgt_lang}.txt"
+		sims = loadtxt(IN_FILE)
+		sums.append(sims.sum())
+		num += sims.shape[0]
 
-	return np.array(accumulator).mean() # make the list a 2D matrix and compute mean of the flattened array (default behavior)
+	return  np.array(sums).sum() / num # make the list a 2D matrix and compute mean of the flattened array (default behavior)
 
 
 def cosine_similarity_table(src_langs, tgt_langs, datasets):
 	pair_means = dict()
 
 	for src_lang in src_langs:
-                for tgt_lang in tgt_langs:
+		for tgt_lang in tgt_langs:
 			pair_means[f'{src_lang}-{tgt_lang}'] = get_mean_cosine_similarity(src_lang, tgt_lang, datasets)
 
 	OUT_FILE = f'tables/cosine_similarity_table.txt'
-	np.savetxt(OUT_FILE, np.array([pair_means.keys(), pair_means.values()]).T, fmt = '%s')
+	results = np.empty(shape = (len(src_langs) * len(tgt_langs), 2), dtype = 'U20')
+	results[:, 0] = list(pair_means.keys())
+	results[:, 1] = list(pair_means.values())
+	np.savetxt(OUT_FILE, results, fmt = '%s')
 
 
-def get_cosine_similarity_histogram(src_lang, tgt_lang, datasets, bins = 10)
+def get_cosine_similarity_histogram(src_lang, tgt_lang, datasets, bins = 10):
 	histogram = np.zeros(shape = (bins,), dtype = 'int')
-	bins_array = np.arange(0, 1, 1 / bins)
 
 	for dataset in datasets:
 		IN_FILE = f"multi30k-dataset-{src_lang}-{tgt_lang}/similarity_{dataset}_{src_lang}_{tgt_lang}.txt"
-		sims = np.loadtxt(IN_FILE, dtype='d')
-		histograms += np.histograms(sims, bins = bins_array)
+		sims = loadtxt(IN_FILE)
+		histogram += np.histogram(sims, range=(0,1), bins = bins)[0]
 
-	OUT_FILE = f"multi30k-dataset-{args.src}-{args.target}/histogram_{src_lang}_{tgt_lang}.txt"
+	OUT_FILE = f"multi30k-dataset-{src_lang}-{tgt_lang}/histogram_{src_lang}_{tgt_lang}.txt"
 	np.savetxt(OUT_FILE, histogram, fmt='%s')
 
 
-def cosine_similarity_histograms(src_langs, tgt_langs, datasets, bins = 10)
+def cosine_similarity_histograms(src_langs, tgt_langs, datasets, bins = 10):
 	for src_lang in src_langs:
 		for tgt_lang in tgt_langs:
 			get_cosine_similarity_histogram(src_lang, tgt_lang, datasets, bins)
@@ -48,20 +55,20 @@ def cosine_similarity_histograms(src_langs, tgt_langs, datasets, bins = 10)
 if __name__ == "__main__":
 	# Usage similarity_analysis_multi30k.py --srcs "en,fr" --tgts "es,ay,gn,qu"
 	parser = argparse.ArgumentParser(
-		prog = "MULTI30K COSINE SIMILARITY ANALYZER"
-		descriptions = "This script analyzes the cosine similarities written to various files by cosine_similarity_multi30k.py. If the similarity or similarities to be analyzed does/do not exist, it will throw an error."
+		prog = "MULTI30K COSINE SIMILARITY ANALYZER",
+		description = "This script analyzes the cosine similarities written to various files by cosine_similarity_multi30k.py. If the similarity or similarities to be analyzed does/do not exist, it will throw an error."
 	)
 	parser.add_argument('-s','--srcs', default='cs,de,en,fr', help="comma separated list of ISO 639-1 codes of source languages for cosine similarity analysis. These will be analyzed pairwise with the target languages (tgts).")
-	parser.add_argument('-t','--tgts', default='es,ay,gn,qu,zh_hans,zh_hant,ar_arab,ar_latn,uk', help = "comma separated list of ISO 639-1 codes of target languages for cosine similarity analysis. These will be analyzed pairwise with the source languages (srcs)."
-	parser.add_argument('-d','--datasets',default='all', help = 'comma separated list of dataset(s) to analyze. Type all as a shortcut to analyze all datasets.'
+	parser.add_argument('-t','--tgts', default='es,ay,gn,qu,zh_hans,zh_hant,ar_arab,ar_latn,uk', help = "comma separated list of ISO 639-1 codes of target languages for cosine similarity analysis. These will be analyzed pairwise with the source languages (srcs).")
+	parser.add_argument('-d','--datasets',default='all', help = 'comma separated list of dataset(s) to analyze. Type all as a shortcut to analyze all datasets.')
 	parser.add_argument('-a','--analysis',default='all', choices=['mean','hist','all'], help='conduct the mean cosine similarity analysis (mean), a histogram analysis (hist), or all analyzes (all).')
 
-	args = parser.parse_args()cosine_similarity_histogram(src_langs, tgt_langs)
+	args = parser.parse_args()
 
-	if args.dataset == 'all':
-		datasets = np.array(['test_2016_flickr', 'test_2017_flickr', 'test_2017_mscoco', 'test_2018_flickr', 'train', 'val'])
-	else
-		datasets = args.dataset.strip().split(',')
+	if args.datasets == 'all':
+		datasets = ['test_2016_flickr', 'test_2017_flickr', 'test_2017_mscoco', 'test_2018_flickr', 'train', 'val']
+	else:
+		datasets = args.datasets.strip().split(',')
 
 	src_langs = args.srcs.strip().split(',')
 	tgt_langs = args.tgts.strip().split(',')
@@ -69,9 +76,9 @@ if __name__ == "__main__":
 	if args.analysis == 'mean':
 		cosine_similarity_table(src_langs, tgt_langs, datasets)
 	elif args.analysis == 'hist':
-		cosine_similarity_histogram(src_langs, tgt_langs, datasets)
+		cosine_similarity_histograms(src_langs, tgt_langs, datasets)
 	elif args.analysis == 'all':
 		cosine_similarity_table(src_langs, tgt_langs, datasets)
-		cosine_similarity_histogram(src_langs, tgt_langs, datasets)
+		cosine_similarity_histograms(src_langs, tgt_langs, datasets)
 
 	# no else block. All options are included in the if and elif blocks.
