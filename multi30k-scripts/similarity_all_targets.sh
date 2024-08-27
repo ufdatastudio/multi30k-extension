dd11fab763b049e76f9b4aadb9d1b8a0f36e415c
CWDE615 <cwellis001@gmail.com>
2024-08-27 11:49:14 -0400

    fix bug in similarity_all_target.sh

--- multi30k-scripts/similarity_all_targets.sh
+++ multi30k-scripts/similarity_all_targets.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # Calculate cosine similarity between the source language in $1 and all relevant target languages.
 # Usage: multi30k-scripts/cosine_similarity_all_targets.sh src
-for tgt in ar_arab es uk zh_hans zh_hant
+for tgt in ar_arab es uk zh_hans zh_hant;
 do
         sbatch --export=src=$1,tgt=$tgt multi30k-scripts/multi30k_similarity.sbatch
 done
